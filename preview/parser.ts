d69c3da2b9e377b473778d176dde8f7efa9c48b0
khai96_ <hvksmr1996@gmail.com>
2020-03-13 20:29:46 +0700

    Create preview/parser.ts

---
+++ preview/parser.ts
@@ -0,0 +1,37 @@
+import build from '../lib/build.ts'
+import { Flag, CountFlag, Option } from '../lib/flags.ts'
+import { FiniteNumber, Integer, Text, Choice } from '../lib/values.ts'
+
+const parser = build()
+  .with(Flag('foo', {
+    alias: ['f'],
+    describe: 'Boolean flag of foo'
+  }))
+  .with(Flag('bar'))
+  .with(CountFlag('count', {
+    alias: ['c'],
+    describe: 'Counting'
+  }))
+  .with(Option('number', {
+    alias: ['N'],
+    type: FiniteNumber,
+    describe: 'An integer or a floating-point number'
+  }))
+  .with(Option('integer', {
+    type: Integer,
+    describe: 'An arbitrary large integer'
+  }))
+  .with(Option('text', {
+    type: Text
+  }))
+  .with(Option('choice', {
+    type: Choice<123 | 'foo' | 456 | 'bar' | '789'>(
+      { value: 123 },
+      { value: 'foo' },
+      { value: 456, describe: 'Not 123' },
+      { value: 'bar', describe: 'Not Foo' },
+      { value: '789', describe: 'Not a number' }
+    )
+  }))
+
+export { parser, parser as default }
