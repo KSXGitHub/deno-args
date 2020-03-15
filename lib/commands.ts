a0662a95d890a95ca45fd1bb16da378055fb0245
khai96_ <hvksmr1996@gmail.com>
2020-03-15 20:15:32 +0700

    Update

--- lib/commands.ts
+++ lib/commands.ts
@@ -34,14 +34,51 @@ type __help = typeof __help
 const __toString = Symbol()
 type __toString = typeof __toString
 
-type CommandTypeBound = MAIN_COMMAND | string
+type CmdTypeBound = MAIN_COMMAND | string
 
 export abstract class CommandBase<
-  CommandType extends CommandTypeBound,
-  Value
+  CmdType extends CmdTypeBound,
+  Val
 > {
-  public abstract readonly commandType: CommandType
-  protected abstract [__parse] (args: readonly ArgvItem[]): ParseResult<Value, readonly FlagError[]>
+  public abstract readonly commandType: CmdType
+  protected abstract [__parse] (args: readonly ArgvItem[]): ParseResult<Val, readonly FlagError[]>
   protected abstract [__help] (): string
   protected abstract [__toString] (): readonly string[]
+
+  public and<NextCmdType extends string, NextVal> (
+    next: CommandBase<NextCmdType, NextVal>
+  ): CommandBase<CmdType, Val & NextVal> {
+    return new Intersection(this, next)
+  }
+
+  public or<NextCmdType extends string, NextVal> (
+    next: CommandBase<NextCmdType, NextVal>
+  ): CommandBase<CmdType, Val | NextVal> {
+    return new Union(this, next)
+  }
+}
+
+const __combine = Symbol()
+type __combine = typeof __combine
+abstract class Combine<AT extends CmdTypeBound, BT extends string, AV, BV, C> extends CommandBase<AT, C> {
+  protected readonly [__combine]: readonly [
+    CommandBase<AT, AV>,
+    CommandBase<BT, BV>
+  ]
+
+  constructor (
+    a: CommandBase<AT, AV>,
+    b: CommandBase<BT, BV>
+  ) {
+    super()
+    this[__combine] = [a, b]
+  }
+}
+
+class Intersection<AT extends CmdTypeBound, BT extends string, AV, BV> extends Combine<AT, BT, AV, BV, AV & BV> {
+
+}
+
+class Union<AT extends CmdTypeBound, BT extends string, AV, BV> extends Combine<AT, BT, AV, BV, AV | BV> {
+
 }
