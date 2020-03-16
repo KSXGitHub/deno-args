255c2e86f5347ca2c7c541aea146830668d6aa50
khai96_ <hvksmr1996@gmail.com>
2020-03-16 15:11:06 +0700

    Update

--- lib/commands.ts
+++ lib/commands.ts
@@ -66,6 +66,12 @@ export abstract class CommandBase<Val> {
       tag: true
     } as const
   }
+
+  public with<NextName extends string, NextVal> (
+    extractor: ArgumentExtractor<NextName, NextVal>
+  ): CommandBase<Val & Record<NextName, NextVal>> {
+    return this.and(new ExtractorWrapper(extractor))
+  }
 }
 
 const __combine = Symbol()
@@ -115,14 +121,14 @@ class Union<A, B> extends Combine<A, B, A | B> {
 
 type TaggedRec<Tag, Name extends string, Val> = Record<command, Tag> & Record<Name, Val>
 
-class ExtractorWrapper<Tag, Name extends string, Val> extends CommandBase<TaggedRec<Tag, Name, Val>> {
+class ExtractorWrapper<Name extends string, Val> extends CommandBase<Record<Name, Val>> {
   constructor (
     private readonly _extractor: ArgumentExtractor<Name, Val>
   ) {
     super()
   }
 
-  protected [__parse] (args: readonly ArgvItem[]): _ParseResult<TaggedRec<Tag, Name, Val>> {
+  protected [__parse] (args: readonly ArgvItem[]): _ParseResult<Record<Name, Val>> {
 
   }
 }
