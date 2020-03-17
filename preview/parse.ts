f022145af4bded4ffb3e6de77aff2ca7af53c0e9
khai96_ <hvksmr1996@gmail.com>
2020-03-17 17:26:08 +0700

    I now know the source of error

--- preview/parse.ts
+++ preview/parse.ts
@@ -39,12 +39,22 @@ for (const args of [
 
   [
     'foo',
-    '--def', 'This is --def'
+    '--def', 'This is --def',
+    '--choice', '123',
+    '--text', 'This is Text',
+    '--integer', '777',
+    '--number', '57.3'
   ],
 
   [
     'bar',
-    '--ghi', 'G'
+    '--ghi', 'G',
+    '--choice', 'foo',
+    '--text', 'This is another piece of text',
+    '--integer', '33',
+    '--number', '57.3',
+    '--foo',
+    '--bar'
   ],
 
   []
