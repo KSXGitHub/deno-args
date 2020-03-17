dcd061d91eee4a93d763ef5a45bb7eaffb93504b
khai96_ <hvksmr1996@gmail.com>
2020-03-17 20:34:02 +0700

    Revelation

--- lib/commands.ts
+++ lib/commands.ts
@@ -1,5 +1,14 @@
-type CommandReturn<Main, Sub extends CommandReturn<any, any>> =
-  CommandReturn.Main<Main> | CommandReturn.Sub<Sub>
+import {
+  ArgvItem,
+  ParseResult,
+  ParseError
+} from './types.ts'
+
+type CommandReturn<
+  Main,
+  Name extends string,
+  Sub extends CommandReturn<any, any, any>
+> = CommandReturn.Main<Main> | CommandReturn.Sub<Name, Sub>
 
 namespace CommandReturn {
   interface Base<Value> {
@@ -9,9 +18,31 @@ namespace CommandReturn {
 
   export interface Main<Value> extends Base<Value> {
     readonly type: 'main'
+    readonly name?: null
   }
 
-  export interface Sub<Value extends CommandReturn<any, any>> extends Base<Value> {
+  export interface Sub<
+    Name extends string,
+    Value extends CommandReturn<any, any, any>
+  > extends Base<Value> {
     readonly type: 'sub'
+    readonly name: Name
   }
 }
+
+export interface Command<
+  Return extends CommandReturn<any, any, any>,
+  ErrList extends readonly ParseError[]
+> {
+  extract (args: readonly ArgvItem[]): ParseResult<Return, ErrList>
+}
+
+export const SubCommand = <
+  Main extends CommandReturn.Main<any>,
+  Sub extends CommandReturn<any, any, any>,
+  MainError extends ParseError,
+  SubError extends ParseError
+> (
+  main: Command<Main, readonly MainError[]>,
+  sub: Command<Sub, readonly SubError[]>
+): Command<Main | Sub, ReadonlyArray<MainError | SubError>> => ({})
