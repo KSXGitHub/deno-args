7b38fbd2cee20ec18110b7085431988c29b3e09a
khai96_ <hvksmr1996@gmail.com>
2020-03-22 20:15:40 +0700

    Implement help

--- lib/help.ts
+++ lib/help.ts
@@ -1,9 +1,34 @@
 import {
-  Command
+  makeIndentN,
+  InitMap
+} from './utils.ts'
+
+import {
+  Command,
+  CommandHelp
 } from './command-types.ts'
 
-export function help (command: Command<any, any>): string {
-
+class HelpCategories extends InitMap<string, CommandHelp[]> {
+  protected init (category: string): CommandHelp[] {
+    return []
+  }
 }
 
+export function * helpLines (command: Command<any, any>): Generator<string, void, unknown> {
+  const catMap = new HelpCategories()
+  for (const item of command.help()) {
+    catMap.get(item.category).push(item)
+  }
+
+  for (const [category, items] of catMap) {
+    yield category + ':'
+
+    for (const { title, description } of items) {
+      yield * makeIndentN(title, 2)
+      if (description) yield * makeIndentN(description, 4)
+    }
+  }
+}
+
+export const help = (command: Command<any, any>): string => [...helpLines(command)].join('\n')
 export default help
