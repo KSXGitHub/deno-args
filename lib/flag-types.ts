c9592ee760c4d7a089bf1b0406a9d89e0b648fd1
khai96_ <hvksmr1996@gmail.com>
2020-03-24 14:55:25 +0700

    Create Partial

--- lib/flag-types.ts
+++ lib/flag-types.ts
@@ -157,3 +157,30 @@ export interface OptionDescriptor<Value> {
   readonly describe?: string
   readonly alias?: readonly string[]
 }
+
+export const Partial = <Name extends string, Value, Default> (
+  x: FlagType<Name, Value>,
+  def: Default,
+  descDef: string = String(def)
+): FlagType<Name, Value | Default> => ({
+  name: x.name,
+  extract (args) {
+    const result = x.extract(args)
+    if (result.tag) return result
+    if (result.error instanceof MissingFlag) {
+      return ok({
+        value: def,
+        consumedFlags: new Set()
+      })
+    }
+    return result
+  },
+  help: once(() => {
+    const { title, description } = x.help()
+    return {
+      title: `${title} [default: ${descDef}]`,
+      description
+    }
+  }),
+  ...sharedProps(`Partial(${x[Symbol.toStringTag]})`)
+})
