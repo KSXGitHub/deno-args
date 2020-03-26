6f52dad8a018484e79bbb2da43a2b6fef9f79001
khai96_ <hvksmr1996@gmail.com>
2020-03-26 16:50:13 +0700

    Create typecheck

---
+++ typecheck/from-preview.ts
@@ -0,0 +1,32 @@
+import { MAIN_COMMAND, PARSE_FAILURE } from '../lib/symbols.ts'
+import { CommandError } from '../lib/command-errors.ts'
+import { ParseError, ArgvItem } from '../lib/types.ts'
+import parser from '../preview/parser.ts'
+import { assert } from './deps.ts'
+
+const result = parser.parse([])
+
+switch (result.tag) {
+  case PARSE_FAILURE:
+    assert<CommandError<readonly ParseError[]>>(result.error)
+    assert<null | undefined>(result.value)
+    break
+
+  case MAIN_COMMAND:
+    assert<ReadonlySet<ArgvItem>>(result.consumedArgs)
+    assert<null | undefined>(result.error)
+    assert<readonly string[]>(result.remaining().rawArgs())
+    assert<readonly string[]>(result.remaining().rawFlags())
+    assert<readonly string[]>(result.remaining().rawValues())
+
+    // flag values
+    assert<void>(result.value.help)
+    assert<boolean>(result.value.foo)
+    assert<boolean>(result.value.bar)
+    assert<'foo' | 'bar' | 123 | 456 | '789'>(result.value.choice)
+    assert<number>(result.value.count)
+    assert<bigint>(result.value.integer)
+    assert<number>(result.value.number)
+    assert<bigint>(result.value["partial-integer"])
+    assert<string>(result.value.text)
+}
