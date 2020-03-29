45944665de7fa2d7b4c806e803d7a440db25f7af
khai96_ <hvksmr1996@gmail.com>
2020-03-29 18:23:41 +0700

    Add tsdoc to CommandError

--- lib/command-errors.ts
+++ lib/command-errors.ts
@@ -6,17 +6,30 @@ import {
   FlagError
 } from './flag-errors.ts';
 
+/**
+ * Class of error created by `CommandType::extract`
+ * @template ErrList Type of array of {@link FlagError}
+ */
 export class CommandError<
   ErrList extends readonly FlagError[]
 > implements ParseError, Iterable<FlagError> {
+  /**
+   * @param errors Array of {@link FlagError}
+   */
   constructor (
     public readonly errors: ErrList
   ) {}
 
+  /**
+   * Get readable error message
+   */
   public readonly toString = () => this.errors
     .map(error => error.toString())
     .join('\n')
 
+  /**
+   * Iterate through `this.errors`
+   */
   public * [Symbol.iterator] () {
     yield * this.errors
   }
