daad1f01363e82771f96111d2486550bf1be62d5
khai96_ <hvksmr1996@gmail.com>
2020-03-29 18:36:27 +0700

    Add tsdoc to flag-errors.ts

--- lib/flag-errors.ts
+++ lib/flag-errors.ts
@@ -12,12 +12,19 @@ import {
 
 const fmtFlagList = (names: readonly string[]) => names.map(flag).join(' ')
 
+/**
+ * Base class of all `FlagError`
+ */
 export abstract class FlagError implements ParseError {
   public abstract toString (): string
 }
 
+/**
+ * `FlagError` class for when unknown flags being detected in argument list
+ */
 export class UnknownFlags extends FlagError {
   constructor (
+    /** Flag names */
     public readonly names: readonly string[]
   ) {
     super()
@@ -28,8 +35,12 @@ export class UnknownFlags extends FlagError {
   }
 }
 
+/**
+ * `FlagError` class for when required flags not being specified
+ */
 export class MissingFlag extends FlagError {
   constructor (
+    /** Flag name */
     public readonly name: string
   ) {
     super()
@@ -40,8 +51,12 @@ export class MissingFlag extends FlagError {
   }
 }
 
+/**
+ * `FlagError` class for when two or more aliases of the same option being specified
+ */
 export class ConflictFlags extends FlagError {
   constructor (
+    /** Aliases */
     public readonly names: readonly string[]
   ) {
     super()
@@ -52,8 +67,12 @@ export class ConflictFlags extends FlagError {
   }
 }
 
+/**
+ * `FlagError` class for when an option flag being specified without a value
+ */
 export class MissingValue extends FlagError {
   constructor (
+    /** Option name */
     public readonly name: string | readonly string[]
   ) {
     super()
@@ -64,9 +83,14 @@ export class MissingValue extends FlagError {
   }
 }
 
+/**
+ * `FlagError` class for when a flag being place where a value is expected
+ */
 export class UnexpectedFlag extends FlagError {
   constructor (
+    /** Option name */
     public readonly name: string | readonly string[],
+    /** Offender flag */
     public readonly unexpectedFlag: string
   ) {
     super()
@@ -77,9 +101,14 @@ export class UnexpectedFlag extends FlagError {
   }
 }
 
+/**
+ * `FlagError` class for when an argument fails to convert to a value
+ */
 export class ValueParsingFailure extends FlagError {
   constructor (
+    /** Option name */
     public readonly name: string | readonly string[],
+    /** Value parsing error */
     public readonly error: ValueError
   ) {
     super()
