0fcc9d12428960d1804241791e0414aa64c25adf
khai96_ <hvksmr1996@gmail.com>
2020-03-29 22:05:38 +0700

    Add tsdoc to BlankReturn and BLANK

--- lib/command-types.ts
+++ lib/command-types.ts
@@ -149,7 +149,9 @@ export interface CommandHelp {
   readonly description?: string
 }
 
+/** Type of {@link BLANK} */
 type BlankReturn = CommandReturn.Main<{}>
+/** Starting point of a command parser construction chain */
 export const BLANK: Command<BlankReturn, never> = ({
   extract: (args) => addExtraProps({
     tag: MAIN_COMMAND,
