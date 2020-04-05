27cbd9719164e2a7211ea5fcf634cbea3837eafe
khai96_ <hvksmr1996@gmail.com>
2020-04-05 21:46:56 +0700

    Create tryExec in utils.ts

--- test/utils.ts
+++ test/utils.ts
@@ -1,2 +1,33 @@
 import { TemplateTag } from './deps.ts'
+
 export const dbg = TemplateTag(Deno.inspect)
+
+export function tryExec<Value, ExpectedError> (
+  fn: () => Value,
+  expectError: (error: unknown) => error is ExpectedError
+): tryExec.Result<Value, ExpectedError> {
+  try {
+    return {
+      tag: true,
+      value: fn()
+    }
+  } catch (error) {
+    if (!expectError(error)) throw error
+    return {
+      tag: false,
+      error
+    }
+  }
+}
+
+export namespace tryExec {
+  export type Result<Value, ExpectedError> = {
+    readonly tag: true
+    readonly value: Value
+    readonly error?: null
+  } | {
+    readonly tag: false
+    readonly error: ExpectedError
+    readonly value?: null
+  }
+}
