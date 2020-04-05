a5fdd673e4a043aeb0008f6129e3ce7e52f05757
khai96_ <hvksmr1996@gmail.com>
2020-04-05 20:41:46 +0700

    Create test for count flag

---
+++ test/wrapper/flags/count-flag.test.ts
@@ -0,0 +1,28 @@
+import { CountFlag } from '../../../lib/flag-types.ts'
+import { MAIN_COMMAND } from '../../../lib/symbols.ts'
+import args from '../../../lib/wrapper.ts'
+import { assertEquals } from '../../deps.ts'
+import { dbg } from '../../utils.ts'
+
+const setup = () => args.with(CountFlag('flag', {
+  alias: ['a', 'b', 'c']
+}))
+
+const testOk = (
+  name: string,
+  argv: readonly string[],
+  expectedValue: unknown
+) => Deno.test(name, () => {
+  const result = setup().parse(argv)
+  if (result.tag !== MAIN_COMMAND) {
+    throw dbg`UnexpectedTag\nresult: ${result}`
+  }
+  assertEquals(result.value, expectedValue)
+})
+
+testOk('no flags', [], { flag: 0 })
+testOk('full name', ['--flag'], { flag: 1 })
+testOk('alias', ['-a'], { flag: 1 })
+testOk('no conflict (grouped)', ['-abc', '--flag'], { flag: 3 + 1 })
+testOk('no conflict (separated)', ['-a', '-b', '-c', '--flag'], { flag: 4 })
+testOk('another', ['-abc', '-a', '-b', '-c', '--flag', '--flag'], { flag: 3 + 3 + 2 })
