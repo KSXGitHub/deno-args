c21ad2e1373618a2e94d0fda8e007190ddf45d4a
khai96_ <hvksmr1996@gmail.com>
2020-04-05 20:38:56 +0700

    Create test for binary flag

---
+++ test/wrapper/flags/binary-flag.test.ts
@@ -0,0 +1,27 @@
+import { BinaryFlag } from '../../../lib/flag-types.ts'
+import { MAIN_COMMAND } from '../../../lib/symbols.ts'
+import args from '../../../lib/wrapper.ts'
+import { assertEquals } from '../../deps.ts'
+import { dbg } from '../../utils.ts'
+
+const setup = () => args.with(BinaryFlag('flag', {
+  alias: ['a', 'b', 'c']
+}))
+
+const testOk = (
+  name: string,
+  argv: readonly string[],
+  expectedValue: unknown
+) => Deno.test(name, () => {
+  const result = setup().parse(argv)
+  if (result.tag !== MAIN_COMMAND) {
+    throw dbg`UnexpectedTag\nresult: ${result}`
+  }
+  assertEquals(result.value, expectedValue)
+})
+
+testOk('no flags', [], { flag: false })
+testOk('full name', ['--flag'], { flag: true })
+testOk('alias', ['-a'], { flag: true })
+testOk('no conflict (grouped)', ['-abc', '--flag'], { flag: true })
+testOk('no conflict (separated)', ['-a', '-b', '-c', '--flag'], { flag: true })
