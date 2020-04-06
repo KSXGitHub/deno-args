0bbc950c6ee828ace1d40fd7ca82bc46eb20cc76
khai96_ <hvksmr1996@gmail.com>
2020-04-06 09:32:41 +0700

    Create test for text

---
+++ test/wrapper/values/text.test.ts
@@ -0,0 +1,26 @@
+import { Option } from '../../../lib/flag-types.ts'
+import { Text } from '../../../lib/value-types.ts'
+import { MAIN_COMMAND, PARSE_FAILURE } from '../../../lib/symbols.ts'
+import args from '../../../lib/wrapper.ts'
+import { assertEquals } from '../../deps.ts'
+import { dbg, fmtTestName } from '../../utils.ts'
+
+const setup = () => args.with(Option('flag', {
+  type: Text
+}))
+
+const testOk = (
+  title: string,
+  argv: readonly string[],
+  expectedValue: unknown
+) => Deno.test(fmtTestName(title, argv), () => {
+  const result = setup().parse(argv)
+  if (result.tag !== MAIN_COMMAND) {
+    throw dbg`unexpected tag\nresult: ${result}`
+  }
+  assertEquals(result.value, expectedValue)
+})
+
+testOk('no whitespace', ['--flag', 'hello'], { flag: 'hello' })
+testOk('with whitespaces', ['--flag', 'hello world'], { flag: 'hello world' })
+testOk('empty', ['--flag', ''], { flag: '' })
