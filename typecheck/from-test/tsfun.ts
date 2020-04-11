46e49da3e6e08c77249db03d9f53c7777755c7fe
khai96_ <hvksmr1996@gmail.com>
2020-04-11 11:55:23 +0700

    Add another type test

--- typecheck/from-test/tsfun.ts
+++ typecheck/from-test/tsfun.ts
@@ -1,4 +1,4 @@
-import { MAIN_COMMAND } from "../../lib/symbols.ts";
+import { MAIN_COMMAND, PARSE_FAILURE } from "../../lib/symbols.ts";
 import * as tsfun from "../../test/tsfun/mixed/setup.ts";
 import * as wrapper from "../../test/wrapper/mixed/setup.ts";
 import { assert } from "../deps.ts";
@@ -8,5 +8,9 @@ const b = wrapper.setup().parse([]);
 assert<typeof b>(a);
 
 if (a.tag === MAIN_COMMAND && b.tag === MAIN_COMMAND) {
+  assert<typeof a>(b);
+}
+
+if (a.tag !== PARSE_FAILURE && b.tag !== PARSE_FAILURE) {
   assert<typeof a>(b);
 }
