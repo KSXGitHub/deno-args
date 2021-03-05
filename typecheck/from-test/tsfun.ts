585e9fefde44bf09af9a08be4fa4b46bbd857b04
khai96_ <hvksmr1996@gmail.com>
2021-03-05 09:11:02 +0700

    Disable tsfun (for now)

--- typecheck/from-test/tsfun.ts
+++ typecheck/from-test/tsfun.ts
@@ -1,18 +1,18 @@
-import { MAIN_COMMAND, PARSE_FAILURE } from '../../lib/symbols.ts'
-import * as tsfun from '../../test/tsfun/mixed/setup.ts'
-import * as wrapper from '../../test/wrapper/mixed/setup.ts'
-import { assert } from '../deps.ts'
+// import { MAIN_COMMAND, PARSE_FAILURE } from '../../lib/symbols.ts'
+// import * as tsfun from '../../test/tsfun/mixed/setup.ts'
+// import * as wrapper from '../../test/wrapper/mixed/setup.ts'
+// import { assert } from '../deps.ts'
 
-const a = tsfun.setup().extract([])
-const b = wrapper.setup().parse([])
-assert<typeof b>(a)
-assert<typeof b.tag>(a.tag)
-assert<typeof a.tag>(b.tag)
+// const a = tsfun.setup().extract([])
+// const b = wrapper.setup().parse([])
+// assert<typeof b>(a)
+// assert<typeof b.tag>(a.tag)
+// assert<typeof a.tag>(b.tag)
 
-if (a.tag === MAIN_COMMAND && b.tag === MAIN_COMMAND) {
-  assert<typeof a>(b)
-}
+// if (a.tag === MAIN_COMMAND && b.tag === MAIN_COMMAND) {
+//   assert<typeof a>(b)
+// }
 
-if (a.tag !== PARSE_FAILURE && b.tag !== PARSE_FAILURE) {
-  assert<typeof a>(b)
-}
+// if (a.tag !== PARSE_FAILURE && b.tag !== PARSE_FAILURE) {
+//   assert<typeof a>(b)
+// }
